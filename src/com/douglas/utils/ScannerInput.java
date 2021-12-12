1f0591bfcb799adf1a61255e22d5b84b8ed7573e
DouglasHLima <douglashdelima@gmail.com>
2021-12-12 20:24:25 -0300

    create nextCharArray methods and overloads for nextIntArray

--- src/com/douglas/utils/ScannerInput.java
+++ src/com/douglas/utils/ScannerInput.java
@@ -17,6 +17,24 @@ public class ScannerInput  {
     public static String[] nextArrayLine(){
         return input.nextLine().split(" ");
     }
+    public static String[] nextArrayLine(int size){
+        String[] line = input.nextLine().split(" ");
+        while (line.length != size){
+            System.out.printf("foi inserido %d valores, é necessário %d valores, tente novamente.%n",
+                    line.length,size);
+            line = input.nextLine().split(" ");
+        }
+        return line;
+    }
+    public static String next(){
+        return input.next();
+    }
+
+    public static char[] nextCharArray(){
+        String word = input.nextLine();
+        return word.toCharArray();
+    }
+
 
     public static int[] nextLineIntArray(){
         String[] lineInputed = nextArrayLine();
@@ -29,9 +47,26 @@ public class ScannerInput  {
             catch (Exception error){
                 System.out.println("Unable to parse string to int: " + error.getMessage());
             }
-
         }
+        return intArray;
+    }
+    public static int[] nextLineIntArray(int size){
+        String[] lineInputed = nextArrayLine();
+        while(lineInputed.length != size){
+            System.out.printf("são necessários %d valores, foram inseridos %d, tente novamente: %n",
+                    size, lineInputed.length);
+            lineInputed = nextArrayLine();
+        }
+        int[] intArray = new int[lineInputed.length];
 
+        for(int i = 0; i < lineInputed.length; i++) {
+            try{
+                intArray[i] = Integer.parseInt(lineInputed[i]);
+            }
+            catch (Exception error){
+                System.out.println("Unable to parse string to int: " + error.getMessage());
+            }
+        }
         return intArray;
     }
 
